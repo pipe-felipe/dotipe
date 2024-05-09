b4126d923979c6bcb2c6d6e4fbd3ad3e1e064d9a
Felipe Alves <104647149+pipe-felipe@users.noreply.github.com>
2024-05-08 22:50:42 -0300

    Add the base functionality (#1)
    This program should download a raw dotfile and put it on the right place
    Also, should see if there any changes on it.

--- dotipe/dotipe_config.py
+++ dotipe/dotipe_config.py
@@ -1,16 +1,17 @@
 from tomllib import load
 from os.path import exists
+from dotipe.utils import TOML_TEXT_BASE
 
 CONFIG_FILE_NAME = "dotipe.toml"
 
 
-class Dotipe:
+class DotipeConfig:
     def __init__(self, configuration_file_path):
         self.config_file_path = configuration_file_path
         self.config_file_name = CONFIG_FILE_NAME
         self.config_file = f"{self.config_file_path}{self.config_file_name}"
 
-    def read_configuration_file(self):
+    def retrieve_data_from_toml(self):
         self.__create_file_if_not_exists()
         with open(self.config_file, "rb") as toml_file:
             configuration = load(toml_file)
@@ -19,4 +20,4 @@ class Dotipe:
     def __create_file_if_not_exists(self):
         if not exists(self.config_file):
             with open(self.config_file, "w") as config_file:
-                config_file.write("[user]\n" 'test = "test"\n')
+                config_file.write(TOML_TEXT_BASE)
---
+++ dotipe/retriver.py
@@ -0,0 +1,46 @@
+from os.path import expanduser
+
+import requests
+
+from dotipe.dotipe_config import DotipeConfig
+
+
+def get_git_remote(url, file_name):
+    try:
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(file_name, "wb") as f:
+                f.write(response.content)
+    except requests.RequestException as e:
+        raise e
+
+
+class Retriever:
+    def __init__(
+        self,
+        dotipe_config: DotipeConfig,
+        section: str,
+        url_key: str,
+        file_path_key: str,
+        file_name_key: str,
+    ):
+        self.dotipe_config = dotipe_config
+        self.home_user_folder = expanduser("~")
+        self.section = section
+        self.url_key = url_key
+        self.file_path_key = file_path_key
+        self.file_name_key = file_name_key
+
+    def get_from_remote(self):
+        user_data = self.dotipe_config.retrieve_data_from_toml()
+        section = user_data[self.section]
+        try:
+            url = section[self.url_key]
+            file = (
+                f"{self.home_user_folder}{user_data[self.section][self.file_path_key]}"
+                f"{user_data[self.section][self.file_name_key]}"
+            )
+            get_git_remote(url, file)
+        except KeyError:
+            print(f"Error: key not found in {self.dotipe_config.config_file_path}")
+            raise
