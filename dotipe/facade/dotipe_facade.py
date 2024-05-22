420d4eb7d6be10008e183bcd5bd04094a834489a
Felipe Alves <alves.n.felipe@gmail.com>
2024-05-21 23:55:58 -0300

    wip

--- dotipe/facade/dotipe_facade.py
+++ dotipe/facade/dotipe_facade.py
@@ -12,3 +12,4 @@ class DotipeFacade:
         if not self.dotipe_config.file_exists():
             self.console.print("The configuration file did not exists :cry:\n", style="bold red")
             self.console.print("I will create a new one for you 😌", style="bold green")
+            self.dotipe_config.create_file_if_not_exists()
--- dotipe/main.py
+++ dotipe/main.py
@@ -2,6 +2,11 @@ from typer import run
 
 from dotipe.cli.dotipe_cli import DotipeCli
 
-if __name__ == "__main__":
+
+def main():
     cli = DotipeCli()
     run(cli.start)
+
+
+if __name__ == "__main__":
+    main()
